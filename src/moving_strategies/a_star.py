5396ebfee499056dc37d84f11a34e0f2389b8e84
zjnT04 <nqt460@gmail.com>
2025-04-17 23:09:08 +0700

    Update logic

--- src/moving_strategies/a_star.py
+++ src/moving_strategies/a_star.py
@@ -13,9 +13,17 @@ def a_star(position, pacman_pos, maze: Map, restricted_cells: list[tuple[int, in
   came_from = {}
   g_score = {position: 0}
   f_score = {position: heuristic(position, pacman_pos)}
+  expanded_nodes = 0  
+  closed_set = set() 
 
   while open_set:
     _, current = heappop(open_set)
+
+    if current in closed_set:
+      continue 
+
+    closed_set.add(current)
+    expanded_nodes += 1 
 
     if current == pacman_pos:
       path = []
@@ -23,10 +31,10 @@ def a_star(position, pacman_pos, maze: Map, restricted_cells: list[tuple[int, in
         path.append(current)
         current = came_from[current]
       path.reverse()
-      return path[0], len(path)
+      return path[0], expanded_nodes
 
     for neighbor in maze.get_neighbors(current):
-      if neighbor in restricted_cells:
+      if neighbor in restricted_cells or neighbor in closed_set:
         continue
 
       tentative_g_score = g_score[current] + 1
@@ -36,4 +44,4 @@ def a_star(position, pacman_pos, maze: Map, restricted_cells: list[tuple[int, in
         f_score[neighbor] = tentative_g_score + heuristic(neighbor, pacman_pos)
         heappush(open_set, (f_score[neighbor], neighbor))
 
-  return position, 0
+  return position, expanded_nodes
